c3b2b50b90c2375f1d14e697f4f389d3cf74153d
wxsatellite <1453085314@qq.com>
2020-04-09 18:29:35 +0800

    添加注释

--- longest-substring-without-repeating-characters/main.go
+++ longest-substring-without-repeating-characters/main.go
@@ -28,7 +28,13 @@ func lengthOfLongestSubstring(s string) (length int) {
 	return
 }
 
-
+/**
+滑动窗口：
+滑动窗口是数组/字符串问题中常用的抽象概念。
+窗口通常是在数组/字符串中由开始和结束索引定义的一系列元素的集合，即 [i, j)（左闭，右开）。
+而滑动窗口是可以将两个边界向某一方向“滑动”的窗口。例如，我们将 [i, j) 向右滑动 1 个元素，
+则它将变为 [i+1, j+1)[i+1,j+1)（左闭，右开）。
+ */
 // 优化
 func lengthOfLongestSubstringOptimize(s string) (max int) {
 	if "" == s {
@@ -56,7 +62,6 @@ func lengthOfLongestSubstringOptimize(s string) (max int) {
 
 
 // 大神解决
-
 func lengthOfLongestSubstringGod(s string) int {
 	var Length int
 	var s1 string
