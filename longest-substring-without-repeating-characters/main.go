2a2aeb5565a324e311daefd21acd1a398f68d0da
wxsatellite <1453085314@qq.com>
2020-04-09 17:44:47 +0800

    无重复字符的最长子串

---
+++ longest-substring-without-repeating-characters/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 暴力破解
+func lengthOfLongestSubstring(s string) (length int) {
+	if "" == s {
+		return
+	}
+	length = 1
+	for i := 0; i < len(s); i++ {
+		set := make(map[uint8]bool)
+		set[s[i]] = true
+		for j := i + 1; j < len(s); j++ {
+			if _, ok := set[s[j]]; ok {
+				break
+			}
+			set[s[j]] = true
+			cMax := j - i + 1
+			if cMax > length {
+				length = cMax
+			}
+		}
+	}
+	return
+}
+
+
+// 优化
+func lengthOfLongestSubstringOptimize(s string) (max int) {
+	if "" == s {
+		return
+	}
+	var (
+		left = -1
+		right = 0
+		set = make(map[uint8]bool)
+	)
+	for right < len(s) {
+		if _, ok := set[s[right]]; ok {
+			left++
+			delete(set,s[left])
+		} else {
+			set[s[right]] = true
+			if right - left > max {
+				max = right - left
+			}
+			right ++
+		}
+	}
+	return
+}
+
+
+// 大神解决
+
+func lengthOfLongestSubstringGod(s string) int {
+	var Length int
+	var s1 string
+	left := 0
+	right := 0
+	s1 = s[left:right]
+	for ; right < len(s); right++ {
+
+		if index := strings.IndexByte(s1, s[right]); index != -1 {
+			left += index + 1
+		}
+		s1 = s[left : right+1]
+		if len(s1) > Length {
+			Length = len(s1)
+		}
+	}
+
+	return Length
+}
+
+func main() {
+	var s = "bbbbb"
+	fmt.Println(lengthOfLongestSubstringOptimize(s))
+}
