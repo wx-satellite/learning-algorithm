9d572b42af8b854ddc7cd6c206a6159ac1b7f166
wxsatellite <1453085314@qq.com>
2020-04-10 20:54:13 +0800

    整数反转

--- 整数反转/main.go
+++ 整数反转/main.go
@@ -6,14 +6,19 @@ func reverse(x int) int {
 	var (
 		result = 0
 	)
+	// x为正负数不用分类讨论。
+	// 因为：-123 % 10 = -3，与正数的处理逻辑一致
 	for x != 0 {
 		mod := x % 10
 		newResult := result*10 + mod
+		// 倒推思路： C = A + B，如果没有溢出 A = C - B，溢出之后 A != C - B
 		if (newResult-mod)/10 != result {
 			return 0
 		}
 
 		result = newResult
+
+		// 去掉最后一位
 		x = x / 10
 
 	}
