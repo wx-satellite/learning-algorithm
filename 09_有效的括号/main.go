4cfa211823564c8bdcaf634e6086e0b3544131c4
wxsatellite <1453085314@qq.com>
2020-04-15 22:59:06 +0800

    有效括号

--- 09_有效的括号/main.go
+++ 09_有效的括号/main.go
@@ -6,15 +6,17 @@ import (
 )
 
 type Stack struct {
-	stack []byte
-	length int
+	stack []byte // 存放字节
+	length int // 内部维护的长度
 }
 
+// 压栈
 func (s *Stack) push(b byte) {
 	s.stack = append(s.stack,b)
 	s.length ++
 }
 
+// 出栈
 func (s *Stack) pop() (res byte) {
 	if s.length <= 0 {
 		return
@@ -25,10 +27,12 @@ func (s *Stack) pop() (res byte) {
 	return
 }
 
+// 判断栈是否为空
 func (s *Stack) isEmpty() bool {
 	return s.length <= 0
 }
 
+// 构造
 func getStack() *Stack {
 	return &Stack{
 	}
@@ -38,20 +42,26 @@ func isValid(s string) bool {
 	if len(s) <= 0 {
 		return true
 	}
+	// 实例化栈
 	stack := getStack()
 	for i := 0; i < len(s); i++ {
+		// 判断是左括号就压入栈
 		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
 			stack.push(s[i])
 		} else {
+			// 如果栈为空，这时候 i 没有越界，则返回 false
 			if stack.isEmpty() {
 				return false
 			}
+			// 获取栈顶元素
 			top := stack.pop()
+			// 比较是否匹配
 			if '(' == top && s[i] != ')' || '[' == top && s[i] !=']' || '{' == top && s[i] !='}'{
 				return false
 			}
 		}
 	}
+	// 如果 i 越界，并且 栈 为空，则返回 true
 	if stack.isEmpty() {
 		return true
 	}
@@ -60,11 +70,17 @@ func isValid(s string) bool {
 
 
 func isValidOther(s string) bool {
+
+	// 判断是否有一对相邻的括号
 	for strings.Index(s,"()") != -1 || strings.Index(s,"[]") != -1 || strings.Index(s,"{}") != -1 {
+
+		// 存在，则替换成 空字符串， 继续下次判断
 		s = strings.Replace(s,"()","",-1)
 		s = strings.Replace(s,"[]","",-1)
 		s = strings.Replace(s,"{}","",-1)
 	}
+
+	// 如果不存在一对相邻的括号，并且剩余的字符串长度不为0，则返回 false
 	if len(s) >= 1 {
 		return false
 	}
