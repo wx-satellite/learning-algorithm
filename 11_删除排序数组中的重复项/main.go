12c332e71de7b8589655ae831f612cfe39712a11
wxsatellite <1453085314@qq.com>
2020-04-17 21:45:20 +0800

    流程图离线补充以及删除排序数组重复元素文章边写

--- 11_删除排序数组中的重复项/main.go
+++ 11_删除排序数组中的重复项/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 
+// nums是有序的，并且是从小到大的
 func removeDuplicates(nums []int) int {
 	if len(nums) <= 0 {
 		return 0
@@ -11,14 +12,18 @@ func removeDuplicates(nums []int) int {
 	j := 1
 
 	for j <= len(nums) -1 {
+
+		// 相等，则 j++
 		if nums[j] == nums[i] {
 			j++
 		} else {
+				// 不想等，则 i+1 与 j 指向的元素互换，i 向后移动一位，j 向后移动一位
 			i++
 			nums[i], nums[j] = nums[j], nums[i]
 			j++
 		}
 	}
+	// 返回长度
 	return i + 1
 }
 
